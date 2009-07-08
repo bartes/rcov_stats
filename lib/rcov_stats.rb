b370ec397441ff06c945d013d729e39a331ed34e
bartes <bartosz.knapik@llp.pl>
2009-07-08 17:14:43 +0200

    fixed path to rspec.yml

--- lib/rcov_stats.rb
+++ lib/rcov_stats.rb
@@ -181,13 +181,13 @@ unless  Object.const_defined?('RCOV_STATS_ROOT')
 end
 
 
-plugin_root = File.dirname(__FILE__)
+file_path = File.dirname(__FILE__)
 config_file = File.join(RCOV_STATS_ROOT,'config','rcov_stats.yml')
 
 unless File.exists?(config_file)
   use_rspec = File.exists?(File.join(RCOV_STATS_ROOT, 'spec'))
   config_file_base = (use_rspec ? 'rcov_rspec' : 'rcov_standard') + '.yml'
-  FileUtils.cp(File.join(plugin_root, 'config', config_file_base), config_file)
+  FileUtils.cp(File.join(file_path, '..','config', config_file_base), config_file)
 end
 
 
